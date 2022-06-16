c9b3871638643e8f3d98038074d8cbde47545960
sakshat <sakshat@classplus.co>
2022-06-16 13:42:13 +0530

    Added functionality for theme switch

--- components/Button/Button.styled.js
+++ components/Button/Button.styled.js
@@ -1,12 +1,6 @@
 import styled from "@emotion/styled";
-import { useContext } from "react";
-import { AppThemeContext } from "../../context/AppThemeContext/AppThemeContext";
 
 export const Button = styled.button(({ theme }) => {
-  const { isDarkMode } = useContext(AppThemeContext);
-
-  console.log("hello", isDarkMode);
-
   return {
     padding: `${theme.spaces[3]} ${theme.spaces[6]}`,
     borderRadius: "4px",
